0dc632545950eb9167f85649c9d839a8f24c9e46
Alexandros Asimakis Atzarakis <50553918+MontreTT@users.noreply.github.com>
2024-03-31 23:47:33 +0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,13 +32,15 @@ ALLOWED_HOSTS = [
     'localhost',
     'https://alexatzaraki-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai',
-    'https://alexatzaraki-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://alexatzaraki-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy'
+    .cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://alexatzaraki-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai',
-    'https://alexatzaraki-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-    
+    'https://alexatzaraki-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai'
+
 ]
 
 REST_FRAMEWORK = {
@@ -148,10 +150,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
-    
+
 ]
