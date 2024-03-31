0fd0e7db718c939e1e75d0af866444bff6206557
Alexandros Asimakis Atzarakis <50553918+MontreTT@users.noreply.github.com>
2024-03-31 12:29:58 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,24 +1,25 @@
-#from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
-#from django.contrib import messages
-#from datetime import datetime
-from .restapis import get_request, analyze_review_sentiments, post_review
+# from django.contrib import messages
+# from datetime import datetime
+from .restapis import get_request, analyze_review_sentiments, # post_review
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 
 logger = logging.getLogger(__name__)
 
+
 @csrf_exempt
 def login_user(request):
     data = json.loads(request.body)
     username = data.get('userName', '')
     password = data.get('password', '')
-    user = authenticate(username=username, password=password)
+    user = authenticate(username=username, password=password) # noqa: F821
     response_data = {"userName": username}
-    
+
     if user is not None:
         login(request, user)
         response_data["status"] = "Authenticated"
@@ -28,9 +29,9 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return JsonResponse({"userName": ""})
+
+
 @csrf_exempt
-
-
 def registration(request):
     data = json.loads(request.body)
     username = data.get('userName', '')
@@ -39,19 +40,21 @@ def registration(request):
     last_name = data.get('lastName', '')
     email = data.get('email', '')
     username_exist = False
-    #email_exist = False
+    # email_exist = False
     try:
         User.objects.get(username=username)
         username_exist = True
     except User.DoesNotExist:
         logger.debug("{} is a new user".format(username))
     if not username_exist:
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, 
-        password=password, email=email)
+        user = User.objects.create_user(username=username, first_name=first_name, 
+                                        last_name=last_name, 
+                                        password=password, email=email)
         login(request, user)
         return JsonResponse({"userName": username, "status": "Authenticated"})
     else:
-        return JsonResponse({"userName": username, "error": "Already Registered"})
+        return JsonResponse({"userName": username, "error": 
+                             "Already Registered"})
 
 
 def get_dealerships(request, state="All"):
@@ -67,7 +70,7 @@ def get_dealer_reviews(request, dealer_id):
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             review_detail['sentiment'] = response['sentiment']
-            
+        
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
@@ -84,11 +87,12 @@ def get_dealer_details(request, dealer_id):
 
 def add_review(request):
     if not request.user.is_anonymous:
-        data = json.loads(request.body)
+        # data = json.loads(request.body)
         try:
-            #response = post_review(data)
+            # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
-            return JsonResponse({"status": 401, "message": f"Error in posting review: {e}"})
+            return JsonResponse({"status": 401, "message": f"Error in 
+            posting review: {e}"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
