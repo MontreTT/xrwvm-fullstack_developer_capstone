603760be341d72c65362e65959da4992667038cf
Alexandros Asimakis Atzarakis <50553918+MontreTT@users.noreply.github.com>
2024-04-03 14:13:38 +0300

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -7,11 +7,12 @@ import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview";
 import { Routes, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} /> {/* Add this line */}
+      <Route path="/register" element={<Register />} /> 
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/postreview/:id" element={<PostReview/>} />
