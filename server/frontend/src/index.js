4207f983fb87faf901952fc2a75d59fcc6698c1c
Alexandros Asimakis Atzarakis <50553918+MontreTT@users.noreply.github.com>
2024-04-03 12:49:08 +0300

    Update index.js

--- server/frontend/src/index.js
+++ server/frontend/src/index.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 8 */
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 import { BrowserRouter } from "react-router-dom";
@@ -9,6 +9,6 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-      <App/>
+        <App/>
     </BrowserRouter>
 );
