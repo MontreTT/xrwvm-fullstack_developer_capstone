7f5668cd7a4c6040874d0be6ef81b9bdbf90e760
Alexandros Asimakis Atzarakis <50553918+MontreTT@users.noreply.github.com>
2024-04-03 12:34:05 +0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = [
     'localhost',
     'https://alexatzaraki-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai',
-    'https://alexatzaraki-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy'
-    '.cognitiveclass.ai',
+    'https://alexatzaraki-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai',
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://alexatzaraki-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
