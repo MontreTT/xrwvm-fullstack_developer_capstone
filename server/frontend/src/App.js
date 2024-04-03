4a902d88b5eab7b087b4675903a1059a470a71e1
Alexandros Asimakis Atzarakis <50553918+MontreTT@users.noreply.github.com>
2024-04-03 12:59:33 +0300

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,10 @@
 /*jshint esversion: 8 */
 
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import Register from './components/Register/Register';
 import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
-import PostReview from "./components/Dealers/PostReview"
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
